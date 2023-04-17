c8b606593d91895385e35cec04ff330508596a72
Marcos Bueno <marcos.filho@facens.br>
2023-04-16 23:30:13 -0300

    create form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -10,14 +10,16 @@ export function Home() {
     <View style={styles.container}>
       <Text style={styles.title}>Tarefas</Text>
       <Text style={styles.date}>Domingo, 16 de Abril de 2023.</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Adicione uma tarefa..."
-        placeholderTextColor="#6B6B6B"
-      />
-      <TouchableOpacity style={styles.button} onPress={handleTaskAdd}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Adicione uma tarefa..."
+          placeholderTextColor="#6B6B6B"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleTaskAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -16,13 +16,21 @@ export const styles = StyleSheet.create({
     color: '#6B6B6B',
     fontSize: 16,
   },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42,
+  },
   input: {
+    flex: 1,
     height: 56,
     backgroundColor: '#1F1E25',
     borderRadius: 5,
     color: '#FFF',
     padding: 16,
     fontSize: 16,
+    marginRight: 12,
   },
   button: {
     width: 56,

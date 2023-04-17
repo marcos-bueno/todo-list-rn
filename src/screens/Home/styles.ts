622f48ddea2d78c2fa57ec25d42116da9a32aa9a
Marcos Bueno <marcos.filho@facens.br>
2023-04-16 22:42:48 -0300

    project organization

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24,
+  },
+  title: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  date: {
+    color: '#6B6B6B',
+    fontSize: 16,
+  },
+});
